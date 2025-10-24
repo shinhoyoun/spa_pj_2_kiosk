46c619dfcf9321b30a306bee1fd6fc1db25a605f
SHY <shy9729@gmail.com>
2025-10-24 12:54:11 +0900

    lv2 구현된거 터미널에 출력되는거 정렬되게 수정

---
+++ src/kiosk/MenuItem.java
@@ -0,0 +1,34 @@
+package kiosk;
+
+public class MenuItem {
+
+    // 속성
+    private String name;
+    private double price;
+    private String description;
+
+
+    // 생성자
+    public MenuItem(String name, double price, String description) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+    }
+
+
+    // 기능
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+}
+
+
